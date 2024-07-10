55c44fb26d04039a3914f5b5907d05ec2a328946
AntonioGMtz <antoniogallegos086@gmail.com>
2024-07-09 23:04:28 -0600

    MutableList / recyclerView Init

--- app/src/main/java/com/example/listas/MainActivity.kt
+++ app/src/main/java/com/example/listas/MainActivity.kt
@@ -5,16 +5,39 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.listas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var userAdapter: UserAdapter
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        //Pasamos un listado que puede cambiarse
+        userAdapter = UserAdapter(getUsers())
+        linearLayoutManager = LinearLayoutManager(this)
+
+        binding.recyclerView.apply {
+            layoutManager = linearLayoutManager
+            adapter = userAdapter
         }
+
+
+    }
+
+
+    private fun getUsers(): MutableList<User>{
+        val users = mutableListOf<User>()
+
+        val antonio = User(1,"Antonio", "Gallegos", "")
+        val jesus = User(2, "Jesus", "Martinez", "")
+
+        return users
     }
 }
--- app/src/main/java/com/example/listas/UserAdapter.kt
+++ app/src/main/java/com/example/listas/UserAdapter.kt
@@ -1,24 +1,41 @@
 package com.example.listas
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.listas.databinding.ItemUserBinding
+import com.example.listas.databinding.ActivityMainBinding
 
 class UserAdapter (private val users: List<User>) : RecyclerView.Adapter<UserAdapter.ViewHolder>(){
+
+    private lateinit var context: Context
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserAdapter.ViewHolder {
-        TODO("Not yet implemented")
+        context = parent.context
+
+        //Iinflar la vista
+        val view= LayoutInflater.from(context).inflate(R.layout.item_user,parent,false)
+
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: UserAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val user = users.get(position)
+        with(holder){
+            binding.tvOrder.text = user.id.toString()
+            binding.tvName.text = user.name
+
+
+
+        }
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = users.size
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        val binding = ItemUserBinding.bind(view)
 
     }
 
-} 
+}
